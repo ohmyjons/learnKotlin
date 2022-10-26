e363ef5d2667148a7019dfac7c22009655085b01
Dohan Pranata <dohanpw@gmail.com>
2022-10-27 01:26:28 +0700

    first commit

---
+++ basic/LambdaExpression.kt
@@ -0,0 +1,22 @@
+fun toUpper(value: String): String = value.toUpperCase()
+
+fun main() {
+
+    val contohLambda: (String, String) -> String = { firstName: String, lastName: String ->
+        val result = "$firstName $lastName"
+        result
+    }
+
+    val result = contohLambda("Eko", "Kurniawan")
+    println(result)
+
+    val sayHello: (String) -> String = {
+        "Hello $it"
+    }
+
+    println(sayHello("Eko"))
+
+//    lambda expression dari funtion yang sudah ada
+    val toUpperCase : (String) -> String = ::toUpper
+    println(toUpperCase("Eko Kurniawan"))
+}
